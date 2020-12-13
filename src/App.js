a9c640bbd4c8c9afe093d97c27230bf6847b0170
Sai Charan <tsaicharan03@gmail.com>
2020-12-13 13:22:53 -0600

    added navigation to menu-items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 
+import {Switch, Route} from 'react-router-dom'
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)
+
+const JacketsPage = () => (
+    <div>
+        <h1>JACKETS PAGE</h1>
+    </div>
+)
+
+const SneakersPage = () => (
+    <div>
+        <h1>SNEAKERS PAGE</h1>
+    </div>
+)
+
+const MensPage = () => (
+    <div>
+        <h1>MENS PAGE</h1>
+    </div>
+)
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+        <Switch>
+        <Route exact path={'/'} component={HomePage}/>
+        <Route exact path={'/hats'} component={HatsPage}/>
+        <Route exact path={'/jackets'} component={JacketsPage}/>
+        <Route exact path={'/sneakers'} component={SneakersPage}/>
+        <Route exact path={'/sneakers'} component={MensPage}/>
+
+        </Switch>
     </div>
   );
 }
